d43622aaae5dc7acc912b81c4751028d7269d2d1
koharu iwasaki <xiaoyangshangtian@gmail.com>
2026-05-27 19:17:33 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,14 @@
+let num =15;
+if (num=num % 3 == 0) {
+  console.log("3の倍数です");
+  }
+
+  if (num=num%5==0){
+    console.log("5の倍数です");
+  }
+
+  if(num=num%3==0 && num=num%5==0){
+    console.log("3と5の倍数です");
+  }
+
+  else{console.log(num);}
